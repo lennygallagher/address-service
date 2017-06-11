5f33084012cf941e0b15429f42dc55f71a5e95ed
lennygallagher <lenny.gallagher@gmail.com>
2017-06-11 11:38:18 +0200

    produce record

--- src/main/java/ch/adesso/address/boundry/AddressResource.java
+++ src/main/java/ch/adesso/address/boundry/AddressResource.java
@@ -1,11 +1,18 @@
 package ch.adesso.address.boundry;
 
 import ch.adesso.address.entity.Address;
+import ch.adesso.address.kafka.KafkaProvider;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
+import javax.ejb.Singleton;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,19 +20,35 @@ import java.util.logging.Logger;
 @Path("address")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Stateless
 public class AddressResource {
-    List<Address> addresses = new ArrayList<>();
-
     final Logger logger = Logger.getLogger(AddressResource.class.getSimpleName());
+
+    @Inject
+    private KafkaProducer<String, String> producer;
 
     @GET
     public Address address(){
-        return new Address("street1", "city1");
+        UUID addressUuid = UUID.randomUUID();
+        UUID partyUuid = UUID.randomUUID();
+        return new Address(addressUuid.toString(),"street1", "city1", partyUuid.toString());
     }
 
     @POST
-    public void create(Address address){
-        addresses.add(address);
-        logger.log(Level.INFO,"adding: "+address.toString());
+    public Address create(Address address){
+        setId(address);
+        produceRecord(address);
+        logger.log(Level.INFO,"Creating: "+address.toString());
+        return address;
+    }
+
+    private void setId(Address address) {
+        UUID uuid = UUID.randomUUID();
+        address.setId(uuid.toString());
+    }
+
+    private void produceRecord(Address address) {
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(KafkaProvider.TOPIC, address.toString());
+        producer.send(producerRecord);
     }
 }
--- src/main/java/ch/adesso/address/entity/Address.java
+++ src/main/java/ch/adesso/address/entity/Address.java
@@ -1,44 +1,18 @@
 package ch.adesso.address.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-/**
- * Created by gallagher on 10.06.2017.
- */
+@Data
+@AllArgsConstructor
+@ToString
 public class Address {
+    private String id;
     private String street;
     private String city;
-
-    public Address() {
-    }
-
-    public Address(String street, String city) {
-        this.street = street;
-        this.city = city;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    @Override
-    public String toString() {
-        return "Address{" +
-                "street='" + street + '\'' +
-                ", city='" + city + '\'' +
-                '}';
-    }
+    private String partyId;
 }
